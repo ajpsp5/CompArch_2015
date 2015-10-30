a08c2f42dda4e2a7c7e56a4cd603c2719e451a31
adam <ajpsp5@gmail.com>
2015-10-29 22:09:47 -0500

    ALU functions Working

--- javascript/ALU_functions.js
+++ javascript/ALU_functions.js
@@ -3,15 +3,31 @@
 
 
 function shiftLeft(value, shifts){
-    return value << shifts;
+    var bitLength = value.length;
+    var retval = BinaryToDecimal(value);
+    retval = DecimalToBinary(retval << shifts);
+    console.log('retval: '+ retval);
+    while(retval.length > bitLength){
+        retval = retval.substr(1);  
+    }
+    return retval;
 }
 
 function shiftRight(value, shifts){
-    return value >> shifts;
+    var bitLength = value.length;
+    var retval = BinaryToDecimal(value);
+    retval = DecimalToBinary(retval >> shifts);
+    while(retval.length < bitLength){
+        retval = '0'+retval;  
+    }
+    return retval;
 }
 
 function or(value1, value2){
-    return value1 | value2;
+    var value1 = BinaryToDecimal(value1);
+    var value2 = BinaryToDecimal(value2);
+    retval = DecimalToBinary(value1 | value2);
+    return retval;
 }
 
 function xor(value1, value2){
@@ -19,16 +35,45 @@ function xor(value1, value2){
 }
 
 function and(value1, value2){
-    return value1 & value2;
+    var bitLen = value1.length;
+    if(value1.length != value2.length){
+        if(value1.length > value2.length){
+            while(value2.length < value1.length)
+                value2 = '0'+value2;
+        }else{
+            bitLen = value2.length;
+            while(value1.length < value2.length)
+                value1 = '0'+value1;
+        }
+    }
+    
+    retval = value1 & value2;
+    
+    if(retval.length != bitLen){
+        while(retval.length != bitLen){
+            retval = '0'+retval;
+        }
+    }
+    
+    return retval;
 }
 
 function add(value1, value2){
+    value1 = BinaryToDecimal(value1);
+    value2 = BinaryToDecimal(value2);
+    var total = value1 + value2;
+    return DecimalToBinary(total);
 }
 
 function sub(value1, value2){
+    value1 = BinaryToDecimal(value1);
+    value2 = BinaryToDecimal(value2);
+    var total = value1 - value2;
+    return DecimalToBinary(total);
 }
 
-
+//var tmp = and(DecimalToBinary(2), DecimalToBinary(6));
+//console.log('Tmp: '+tmp);
 
 
 
