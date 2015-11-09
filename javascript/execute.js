a3c8acea411b8620027278621ddda1146ccfa02c
ajpsp5 <ajpsp5@gmail.com>
2015-11-08 19:45:26 -0600

    Added Save Word etc to execute

--- javascript/execute.js
+++ javascript/execute.js
@@ -8,48 +8,110 @@ function jmp(address){
 	fetch();
 }
 
-function ble(){
+function ble(rs, rt, address){
+    rs = BinaryToDecimal(rs);
+    rt = BinaryToDecimal(rt);
+    
+    if(rs <= rt){
+        PC = address;
+        fetch();
+        return;
+    }    
 }
 
-function bgt(){
+function bgt(rs, rt, address){
+    rs = BinaryToDecimal(rs);
+    rt = BinaryToDecimal(rt);
+    
+    if(rs > rt){
+        PC = address;
+        fetch();
+        return;
+    }
 }
 
-function blt(){
+function blt(rs, rt, address){
+    rs = BinaryToDecimal(rs);
+    rt = BinaryToDecimal(rt);
+    
+    if(rs < rt){
+        PC = address;
+        fetch();
+        return;
+    }
 }
 
-function bge(){
+function bge(rs, rt, address){
+    rs = BinaryToDecimal(rs);
+    rt = BinaryToDecimal(rt);
+    
+    if(rs >= rt){
+        PC = address;
+        fetch();
+        return;
+    }
 }
 
-function beq(){
+function beq(rs, rt, address){
+    rs = BinaryToDecimal(rs);
+    rt = BinaryToDecimal(rt);
+    
+    if(rs == rt){
+        PC = address;
+        fetch();
+        return;
+    }
 }
 
-function add(rd, rs, rt){
+function executeAdd(rd, rs, rt){
     fileRegister[rd] = add(fileRegister[rs], fileRegister[rt]);
+    fetch();
 }
 
-function sub(rd, rs, rt){
+function executeSub(rd, rs, rt){
 	fileRegister[rd] = sub(fileRegister[rs], fileRegister[rt]);
+	fetch();
 }
 
-function addi(){
+function executeAddi(rd, rs, value){
+    fileRegister[rd] = addi(fileRegister[rs], value);
+    fetch();
 }
 
-function srl(){
+function srl(rd, rs, rt){
+    fileRegister[rd] = shiftRight(fileRegister[rs], fileRegister[rt]);
+    fetch();
 }
 
 function sll(){
+    fileRegister[rd] = shiftLeft(fileRegister[rs], fileRegister[rt]);
+    fetch();
 }
 
-function or(rd, rs, rt){
+function executeOr(rd, rs, rt){
 	fileRegister[rd] = or(fileRegister[rs], fileRegister[rt]);
+	fetch();
 }
 
-function lw(){
+function lw(rd, rs, address){
+    if(rs)
+    if(BinaryToDecimal(rs) != 0)
+        fileRegister[rd] = memRegister[add(address,  fileRegister[rs])];
+    else
+        fileRegister[rd] = memRegister[address];
+    fetch();
 }
 
-function sw(){
+function sw(rd, rs, address){
+    if(rs)
+    if(BinaryToDecimal(rs) != 0)
+        memRegister[add(address,  fileRegister[rs])] = fileRegister[rd];
+    else
+        memRegister[address] = fileRegister[rd];
+    fetch();
 }
 
 function xor(rd, rs, rt){
 	fileRegister[rd] = xor(fileRegister[rs], fileRegister[rt]);
+	fetch();
 }
